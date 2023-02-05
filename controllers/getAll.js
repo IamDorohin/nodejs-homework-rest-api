c11e8ad15dd519a6a87a0de11a7b39cbe756d5e2
IamDorohin <iamdorohin@gmail.com>
2023-02-05 22:33:30 +0200

    Added filtration favorite contacts

--- controllers/getAll.js
+++ controllers/getAll.js
@@ -1,15 +1,23 @@
-const e = require("cors");
 const Contact = require("../models/contact");
 
 const getAll = async (req, res) => {
   const { _id } = req.user;
-  const { page = 1, limit = 10 } = req.query;
+  const { page = 1, limit = 10, favorite } = req.query;
   const skip = (page - 1) * limit;
 
-  const result = await Contact.find({ owner: _id }, "", {
-    skip,
-    limit: Number(limit),
-  });
+  let result;
+
+  const favoriteContacts = favorite === "true";
+
+  favoriteContacts
+    ? (result = await Contact.find({ owner: _id, favorite: true }, "", {
+        skip,
+        limit: Number(limit),
+      }))
+    : (result = await Contact.find({ owner: _id }, "", {
+        skip,
+        limit: Number(limit),
+      }));
 
   res.json(result);
 };
